7ada6d040a82156d9fafdb378a9e6059eaa7a87b
MUNIF <munif@shellbeehaken.com>
2024-08-22 18:31:57 +0600

    v2 update analyze-passport for passport verification for porichoy api basic route

--- server.js
+++ server.js
@@ -537,8 +537,56 @@ app.post("/upload-passport", async (req, res) => {
   }
 });
 
-// Endpoint to analyze document using Textract
 app.post("/analyze-passport", async (req, res) => {
+  console.log("Endpoint /analyze-passport was hit.");
+  const formatDate = (yyMMdd) => {
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    const year = parseInt(yyMMdd.substring(0, 2), 10);
+    const month = parseInt(yyMMdd.substring(2, 4), 10) - 1;
+    const day = parseInt(yyMMdd.substring(4, 6), 10);
+
+    const fullYear = year >= 50 ? 1900 + year : 2000 + year;
+    return `${day} ${monthNames[month]} ${fullYear}`;
+  };
+
+  const extractMRZFields = (mrz) => {
+    return {
+      passportNumber: mrz.substring(0, 9),
+      passportNumberCheckDigit: parseInt(mrz[9]),
+      dateOfBirth: mrz.substring(13, 19),
+      birthDateCheckDigit: parseInt(mrz[19]),
+      expirationDate: mrz.substring(21, 27),
+      expirationDateCheckDigit: parseInt(mrz[27]),
+      personalNumber: mrz.substring(28, 42),
+      personalNumberCheckDigit: parseInt(mrz[42]),
+      finalCheckDigit: parseInt(mrz[43]),
+    };
+  };
+
+  // Function to get only required fields for response
+  const getRequiredFields = (mrzFields) => {
+    return {
+      passportNumber: mrzFields.passportNumber,
+      dob: formatDate(mrzFields.dateOfBirth),
+      expirationDate: formatDate(mrzFields.expirationDate),
+      nid: mrzFields.personalNumber.substring(0, 10),
+    };
+  };
+
   const { imageUrl } = req.body;
 
   if (!imageUrl) {
@@ -546,47 +594,48 @@ app.post("/analyze-passport", async (req, res) => {
   }
 
   const params = {
-    DocumentPages: [
-      {
-        S3Object: {
-          Bucket: process.env.YOUR_S3_BUCKET_NAME,
-          Name: imageUrl.split("/").pop(),
-        },
+    Document: {
+      S3Object: {
+        Bucket: process.env.YOUR_S3_BUCKET_NAME,
+        Name: imageUrl.split("/").pop(),
       },
-    ],
+    },
   };
 
   try {
-    const response = await textract.analyzeID(params).promise();
-    const identityDocumentFields =
-      response.IdentityDocuments[0].IdentityDocumentFields;
+    const textractData = await textract.detectDocumentText(params).promise();
+    const jsonData = textractData.Blocks;
 
-    let mrzCodeText = null;
-    for (let field of identityDocumentFields) {
-      if (field?.Type?.Text === "MRZ_CODE") {
-        let mrzCodeFull = field?.ValueDetection?.Text;
-        console.log("mrzCodeText" + mrzCodeFull);
-        if (mrzCodeFull) {
-          const mrzLines = mrzCodeFull.split("\n");
-          const mrzLastLine = mrzLines[mrzLines.length - 1];
-          mrzCodeText = mrzLastLine;
-          break;
-        }
+    // Filter blocks to get lines of text
+    const lineBlocks = jsonData.filter((block) => block.BlockType === "LINE");
+
+    let name = null; // Variable to store the name value
+
+    let nameBlockIndex = -1;
+
+    // Find the index of the block that includes the text "Name"
+    lineBlocks.forEach((block, index) => {
+      if (block.Text.includes("Name")) {
+        nameBlockIndex = index;
       }
+    });
+    if (nameBlockIndex !== -1 && nameBlockIndex < lineBlocks.length - 1) {
+      const nextBlock = lineBlocks[nameBlockIndex + 1];
+      name = nextBlock.Text;
     }
+    const lastLineBlock = lineBlocks[lineBlocks.length - 1];
+
+    // Access the Text value and set it to a variable named mrzCodeText
+    const mrzCodeText = lastLineBlock.Text;
 
     if (mrzCodeText) {
-      const result = validateMRZ(mrzCodeText);
-
-      // Extract all MRZ fields
       const mrzFields = extractMRZFields(mrzCodeText);
-
-      // Use only the required fields for response
       const responseData = getRequiredFields(mrzFields);
 
+      responseData.name = name;
+
       res.json({
-        responseData,
-        passportVerificationStatus: result.isPassportValid,
+        data: responseData,
       });
     } else {
       res.status(404).send("MRZ code not found.");
@@ -596,157 +645,6 @@ app.post("/analyze-passport", async (req, res) => {
     res.status(500).send("An error occurred while analyzing the document.");
   }
 });
-const formatDate = (yyMMdd) => {
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  const year = parseInt(yyMMdd.substring(0, 2), 10);
-  const month = parseInt(yyMMdd.substring(2, 4), 10) - 1;
-  const day = parseInt(yyMMdd.substring(4, 6), 10);
-
-  const fullYear = year >= 50 ? 1900 + year : 2000 + year;
-  return `${day} ${monthNames[month]} ${fullYear}`;
-};
-
-const getCharacterValue = (char) => {
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  if (char >= "0" && char <= "9") {
-    return parseInt(char);
-  } else if (char >= "A" && char <= "Z") {
-    return alphabet.indexOf(char) + 10;
-  } else {
-    return 0; // Placeholder < is treated as 0
-  }
-};
-
-const calculateCheckDigit = (input) => {
-  const weights = [7, 3, 1];
-  let sum = 0;
-
-  for (let i = 0; i < input.length; i++) {
-    const value = getCharacterValue(input[i]);
-    sum += value * weights[i % 3];
-  }
-
-  return sum % 10;
-};
-
-const parseDate = (yyMMdd) => {
-  const year = parseInt(yyMMdd.substring(0, 2), 10);
-  const month = parseInt(yyMMdd.substring(2, 4), 10) - 1;
-  const day = parseInt(yyMMdd.substring(4, 6), 10);
-
-  const fullYear = year >= 50 ? 1900 + year : 2000 + year;
-
-  return new Date(fullYear, month, day);
-};
-
-const extractMRZFields = (mrz) => {
-  return {
-    passportNumber: mrz.substring(0, 9),
-    passportNumberCheckDigit: parseInt(mrz[9]),
-    birthDate: mrz.substring(13, 19),
-    birthDateCheckDigit: parseInt(mrz[19]),
-    expirationDate: mrz.substring(21, 27),
-    expirationDateCheckDigit: parseInt(mrz[27]),
-    personalNumber: mrz.substring(28, 42),
-    personalNumberCheckDigit: parseInt(mrz[42]),
-    finalCheckDigit: parseInt(mrz[43]),
-  };
-};
-
-// Function to get only required fields for response
-const getRequiredFields = (mrzFields) => {
-  return {
-    passportNumber: mrzFields.passportNumber,
-    birthDate: formatDate(mrzFields.birthDate),
-    expirationDate: formatDate(mrzFields.expirationDate),
-    personalNumber: mrzFields.personalNumber.substring(0, 10),
-  };
-};
-
-const validateMRZ = (mrz) => {
-  console.log("ValidateMRZ" + mrz);
-  if (!mrz) {
-    // console.error("MRZ code is null or undefined");
-    return {
-      isPassportNumberValid: false,
-      isBirthDateValid: false,
-      isExpirationDateValid: false,
-      isPersonalNumberValid: false,
-      isFinalCheckDigitValid: false,
-      isExpirationDateNotExpired: false,
-      isPassportValid: false,
-    };
-  }
-  const {
-    passportNumber,
-    passportNumberCheckDigit,
-    birthDate,
-    birthDateCheckDigit,
-    expirationDate,
-    expirationDateCheckDigit,
-    personalNumber,
-    personalNumberCheckDigit,
-    finalCheckDigit,
-  } = extractMRZFields(mrz);
-
-  const isPassportNumberValid =
-    calculateCheckDigit(passportNumber) === passportNumberCheckDigit;
-  const isBirthDateValid =
-    calculateCheckDigit(birthDate) === birthDateCheckDigit;
-  const isExpirationDateValid =
-    calculateCheckDigit(expirationDate) === expirationDateCheckDigit;
-  const isPersonalNumberValid =
-    calculateCheckDigit(personalNumber) === personalNumberCheckDigit;
-
-  const expirationDateObj = parseDate(expirationDate);
-  const currentDate = new Date();
-  const isExpirationDateNotExpired = expirationDateObj >= currentDate;
-
-  const combined =
-    passportNumber +
-    passportNumberCheckDigit +
-    birthDate +
-    birthDateCheckDigit +
-    expirationDate +
-    expirationDateCheckDigit +
-    personalNumber +
-    personalNumberCheckDigit;
-  const isFinalCheckDigitValid =
-    calculateCheckDigit(combined) === finalCheckDigit;
-
-  const isPassportValid =
-    isPassportNumberValid &&
-    isBirthDateValid &&
-    isExpirationDateValid &&
-    isPersonalNumberValid &&
-    isFinalCheckDigitValid &&
-    isExpirationDateNotExpired;
-
-  return {
-    isPassportNumberValid,
-    isBirthDateValid,
-    isExpirationDateValid,
-    isPersonalNumberValid,
-    isFinalCheckDigitValid,
-    isExpirationDateNotExpired,
-    isPassportValid,
-  };
-};
 
 // Start server
 app.listen(port, () => {
